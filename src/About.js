b337c32dc1aee7c56635b582572767cc1319ab47
Mike Cornish <cornishmw@gmail.com>
2016-09-29 12:55:04 -0400

    Added About content and some styles.

--- src/About.js
+++ src/About.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import './About.css';
 
-const About = (props) => (
-  <section id="about">
-      <h1>Hi, my name is Mike.</h1>
-      <p>Content here.</p>
-  </section>
-)
+const About = (props) => {
+    const content = () => (
+        <div className="bio">
+            <p>I'm a compulsive creator. When I'm not designing and developing websites, I like to write stories and blog posts, draw and sketches and art, and occasionally create music. My main passion is learning, because it allows me to execute bigger, better, more ambitious creations.</p>
+
+            <p>Some of my most notable accompliishments include working on a Best in Show award-winning website, writing two novels (unpublished), interviewing a chart-topping musician, and writing hundreds of blog posts over the past six years.</p>
+
+            <p>Mostly I just play with JavaScript, though.</p>
+        </div>
+    );
+
+    return (
+        <section id="about">
+            <div className="h-card">
+                <h1>Hi, my name is <span className="p-name">Mike Cornish</span>.</h1>
+                {content()}
+            </div>
+        </section>
+    );
+}
 
 export default About;
--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Nav from './Nav';
 import About from './About';
