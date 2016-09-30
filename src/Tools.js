f49d23cf7c76ac40a7e15cee852f0185aba9da84
Mike Cornish <cornishmw@gmail.com>
2016-09-29 20:34:45 -0400

    Added tool icons (in progress)

--- src/Tools.js
+++ src/Tools.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import tools from './tools-data.js';
+import './Tools.css';
 
 const Tools = (props) => (
     <div role="grid">
         {tools.map(tool => (
-            <div role="gridcell">{tool.name}</div>
+            <div role="gridcell" className="tool-icon">{tool.icon ? tool.icon() : tool.name}</div>
         ))}
     </div>
 )
