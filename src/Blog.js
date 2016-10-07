9b9c17ccd1e052d7feb19fb5af05ccb9ebf6f9dc
Mike Cornish <cornishmw@gmail.com>
2016-10-07 14:44:56 -0400

    Adds content for "My Story" section

--- src/Blog.js
+++ src/Blog.js
@@ -4,8 +4,8 @@ import './Blog.css';
 const Blog = (props) => {
     const content = () => (
         <div className="excerpt">
-            <p>Blog post excerpt content goes here.</p>
-            <a role="button" href="http://blog.mikecornish.net" target="_blank">Read More</a>
+            <p>I remember the moment I felt like I might actually belong. Before that moment, I was never fully aware of the importance of community in web development. For years, I was just a web developer; after that moment, I was part of the web development community.</p>
+            <a role="button" href="http://devstory.mikecornish.net/posts/chapter-5" target="_blank">Read More</a>
         </div>
     );
 
