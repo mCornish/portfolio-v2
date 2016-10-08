c8c6dd72266a5508f2bf370a90b6d9fe2edc8e9d
Mike Cornish <cornishmw@gmail.com>
2016-10-08 15:07:10 -0400

    Styles many components

--- src/About.js
+++ src/About.js
@@ -4,7 +4,7 @@ import './About.css';
 const About = (props) => {
     const content = () => (
         <div className="bio">
-            <p>I'm a compulsive creator. When I'm not designing and developing websites, I like to write stories and blog posts, draw and sketches and art, and occasionally create music. My main passion is learning, because it allows me to execute bigger, better, more ambitious creations.</p>
+            <p>When I'm not designing and developing websites, I like to write stories and blog posts, draw and sketches and art, and occasionally create music. My main passion is learning, because it allows me to execute bigger, better, more ambitious creations.</p>
 
             <p>Some of my most notable accompliishments include working on a Best in Show award-winning website, writing two novels (unpublished), interviewing a chart-topping musician, and writing hundreds of blog posts over the past six years.</p>
 
@@ -15,7 +15,7 @@ const About = (props) => {
     return (
         <section id="about">
             <div className="h-card">
-                <h1>Hi, my name is <span className="p-name">Mike Cornish</span>.</h1>
+                <h1><span className="p-name">Mike Cornish</span> is a compulsive creator.</h1>
                 {content()}
             </div>
         </section>
