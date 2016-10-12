d8708b217022c5bfbb7df23a8235bff78de33a1c
Mike Cornish <cornishmw@gmail.com>
2016-10-12 14:03:36 -0400

    Fixes scroll issue in #5

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -5,16 +5,21 @@ import Logo from './Logo';
 
 const _handleScroll = () => {
     const scrollPos = $(document).scrollTop();
-    $('nav a').each(function() {
-        const $currLink = $(this);
-        const $refEl = $( $currLink.attr('href') );
-        if ($refEl.offset().top <= scrollPos + 200 && $refEl.offset().top + $refEl.height() > scrollPos) {
-            $('nav a').removeClass('is-active');
-            $currLink.addClass('is-active');
-        } else {
-            $currLink.removeClass('is-active');
-        }
-    });
+    if ($(window).scrollTop() + $(window).height() === $(document).height()) {
+        $('nav a').removeClass('is-active');
+        $('nav a:last-child').addClass('is-active');
+    } else {
+        $('nav a').each(function() {
+            const $currLink = $(this);
+            const $refEl = $( $currLink.attr('href') );
+            if ($refEl.offset().top <= scrollPos + 200 && $refEl.offset().top + $refEl.height() > scrollPos) {
+                $('nav a').removeClass('is-active');
+                $currLink.addClass('is-active');
+            } else {
+                $currLink.removeClass('is-active');
+            }
+        });
+    }    
 };
 
 const _smoothScroll = e => {
