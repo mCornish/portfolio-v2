48bfd728d37a51f1914ae7966e61b2bb1c03582c
Mike Cornish <cornishmw@gmail.com>
2016-10-12 18:44:09 -0400

    Continues to update styles

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -5,18 +5,17 @@ import './Blog.css';
 const content = () => (
     <div className="excerpt">
         <p>I remember the moment I felt like I <em>might</em> actually belong. Before that moment, I was never fully aware of the importance of community in web development. For years, I was just a web developer; after that moment, I was part of the web development community.</p>
-        <a role="button" href="http://devstory.mikecornish.net/posts/chapter-5" target="_blank">Read More</a>
+        <a role="button" href="http://devstory.mikecornish.net/posts/chapter-5" target="_blank">Explore My Story</a>
     </div>
 );
 
 
 export default function(props) {
     return (
-        <section id="story">
-            <div className="blog-post">
-                <h2>My Story</h2>
-                {content()}
-            </div>
+        <section id="story" className="blog-post">
+            <h2 className="blog-post__chapter">Chapter 5 <span className="blog-post__title">The New Kid (Again)</span></h2>
+            <h3 className="blog-post__subtitle">Remembering How to Be a <span className="blog-post__highlight">Web Developer</span></h3>
+            {content()}
         </section>
     );
 }
--- src/Contact.jsx
+++ src/Contact.jsx
@@ -6,6 +6,7 @@ const Contact = props => {
         <section id="contact" className="contact">
             <h2>Let's Talk</h2>
             <p>If you're interested in working together, have a question about web development, or just want to say hi, feel free to send me an email at <a href="mailto:cornishmw@gmail.com">cornishmw@gmail.com</a>. Just let me know you came from my website and I'll get back to you within 24 hours.</p>
+            <a role="button" href="mailto:cornishmw@gmail.com">Email Me</a>
         </section>
     );
 }
