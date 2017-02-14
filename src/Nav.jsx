a9e23af0ec5090bd03dab7a63a24759bfc2f6d7a
Mike Cornish <cornishmw@gmail.com>
2017-02-13 20:39:42 -0500

    Updates Nav

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -6,7 +6,21 @@ import scrollToContainer from './scrollToContainer';
 
 import { mixpanel } from './App';
 
-const _handleScroll = () => {
+const Nav = () =>
+    <nav>
+        <Logo />
+        <a href="#about" onClick={handleClick}>Me</a>
+        <a href="#projects" onClick={handleClick}>Projects</a>
+        <a href="#writing" onClick={handleClick}>Writing</a>
+        <a href="#design" onClick={handleClick}>Design</a>
+        <a href="#contact" onClick={handleClick}>Contact Me</a>
+    </nav>
+
+export default Nav;
+
+$(document).on('scroll', handleScroll);
+
+function handleScroll () {
     const scrollPos = $(document).scrollTop();
     if ($(window).scrollTop() + $(window).height() === $(document).height()) {
         $('nav a').removeClass('is-active');
@@ -23,7 +37,7 @@ const _handleScroll = () => {
             }
         });
     }    
-};
+}
 
 const nextLink = () => {
     const $activeLink = $('nav .is-active');
@@ -46,7 +60,7 @@ function queryToHTML($selection) {
     return $.parseHTML($selection[0].outerHTML)[0];
 }
 
-const handleKeydown = e => {
+function handleKeydown (e) {
     if (e.which === 37 || e.which === 38) {
         if (prevLink().length > 0) {
             const link = queryToHTML(prevLink());
@@ -62,32 +76,21 @@ const handleKeydown = e => {
 $(document).on('keydown', handleKeydown);
 
 
-const _smoothScroll = e => {
+function smoothScroll (e) {
     e.preventDefault();
     $(document).off('scroll');
 
     $('nav a').removeClass('is-active');
     $(e.target).addClass('is-active');
     scrollToContainer(e.target);
-};
+}
 
-const _handleClick = e => {
+function handleClick (e) {
     $('nav a').removeClass('is-active');
     $(e.target).addClass('is-active');
-    _smoothScroll(e);
+    smoothScroll(e);
     mixpanel.track('Link Click', {
         'name': $(e.target).text()
     });
-};
+}
 
-$(document).on('scroll', _handleScroll);
-const Nav = () =>
-    <nav>
-        <Logo />
-        <a href="#about" onClick={_handleClick}>About Me</a>
-        <a href="#story" onClick={_handleClick}>My Story</a>
-        <a href="#experience" onClick={_handleClick}>My Experience</a>
-        <a href="#contact" onClick={_handleClick}>Contact Me</a>
-    </nav>
-
-export default Nav;
