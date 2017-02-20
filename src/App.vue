07f76b78b8f876867c089937ef8bd48ee8adc7fd
Mike Cornish <cornishmw@gmail.com>
2017-02-19 20:31:08 -0500

    Adds base styles

--- src/App.vue
+++ src/App.vue
@@ -2,37 +2,176 @@
   <div id="app">
     <nav></nav>
     <main id="main">
-      <about></about>
-      <blog></blog>
-      <experience></experience>
-      <writing></writing>
-      <contact></contact>
+    <about></about>
+    <blog></blog>
+    <experience></experience>
+    <writing></writing>
+    <contact></contact>
     </main>
   </div>
 </template>
 
 <script>
-import Nav from './components/Nav'
-import About from './components/About'
-import Blog from './components/Blog'
-import Experience from './components/Experience'
-import Writing from './components/Writing'
-import Contact from './components/Contact'
-import Footer from './components/Footer'
+  import Nav from './components/Nav'
+  import About from './components/About'
+  import Blog from './components/Blog'
+  import Experience from './components/Experience'
+  import Writing from './components/Writing'
+  import Contact from './components/Contact'
+  import Footer from './components/Footer'
 
-import mixpanel from 'mixpanel-browser'
+  import mixpanel from 'mixpanel-browser'
 
-mixpanel.init('5fbd1f3532c9111fee31a61c8132bc25')
-mixpanel.track('Page View')
-export { mixpanel }
+  mixpanel.init('5fbd1f3532c9111fee31a61c8132bc25')
+  mixpanel.track('Page View')
+  export { mixpanel }
 
-export default {
-  name: 'app',
-  components: {
-    Nav, About, Blog, Experience, Writing, Contact, Footer
+  export default {
+    name: 'app',
+    components: {
+      Nav, About, Blog, Experience, Writing, Contact, Footer
+    }
   }
-}
 </script>
 
 <style lang="sass">
+  @import './styles/variables'
+
+
+  body
+    position: relative
+    margin: 0
+    padding: 0
+    font-family: $font-stack-1
+
+
+  h1, h2, p, a
+    line-height: 1.48
+
+  h1, h2
+    font-family: $font-stack-2
+    margin-top: 0;
+  * + h1, * + h2
+    margin-top: 1em;
+
+  h1
+    font-size: 3rem
+    line-height: 1.3
+    margin-bottom: .5em
+
+  h2
+    font-size: 2.5rem
+    margin-bottom: .5em
+
+  h3
+    margin-top: 2em
+
+  p, a
+    font-size: 1rem
+
+  a
+    color: $color-brand-1
+  a:hover
+    color: $color-brand-2
+
+  a:visited
+    color: rgb(160,180,210)
+
+
+  p
+    margin-bottom: 1em
+
+  td
+    padding: .5em 1em
+
+  main
+    position: relative
+    @media screen and (min-width: 1px)
+      width: 100vw
+    @media screen and (min-width: $break-tablet)
+      width: 80vw /* 100vw - 15vw Nav */
+    @media screen and (min-width: $break-desktop)
+      width: 85vw /* 100vw - 15vw Nav */
+
+  section
+    max-width: 100%
+
+  section + section
+    margin-top: 5em
+
+  section:first-child
+    padding-top: 1em
+
+  @media screen and (min-width: 1px)
+    section
+      padding: 1em
+
+  @media screen and (min-width: 768px)
+    section
+      padding-left: 5vw
+      padding-right: 5vw
+
+  @media screen and (min-width: 1366px)
+    section
+      padding-left: 20vw
+      padding-right: 20vw
+
+  [role=button],
+  a[role=button]
+    display: block
+    width: 100%
+    color: $color-text-light
+    width: 100%
+    color: $color-text-light
+    border: 3px solid $color-text-light
+    padding: 1em
+    margin: 0 auto
+    border-radius: 10em
+    font-weight: bold
+    font-size: 1.5em;
+    text-align: center
+    text-decoration: none
+    text-transform: capitalize
+    transition: color .2s, background-color .2s
+
+  [role=button]:hover
+    color: $color-text-light
+    background-color: $color-brand-2
+    border-color: $color-brand-2
+
+  * + [role=button],
+  * + a[role=button]
+    margin-top: 2em
+
+
+  [role=grid]
+    display: flex
+    flex-wrap: wrap
+    justify-content: center
+
+  [role=gridcell]
+    margin: 1em
+
+  a[role=gridcell]
+    text-decoration: none
+    color: inherit
+
+
+
+  /* UTILITIES */
+  .flex-row
+    display: flex !important
+    flex-direction: row !important
+
+  .flex-center-row
+    display: flex
+    justify-content: center
+
+  .flex-center-col
+    display: flex
+    flex-direction: column
+    align-items: center
+
+  .text-center
+    text-align: center !important
 </style>
--- src/components/About.vue
+++ src/components/About.vue
@@ -1,11 +1,101 @@
 <template>
+  <section id="about" className="about">
+    <div className="h-card">
+      <h1><span className="p-name">Mike Cornish</span> is a</h1>
+      <h2>Frontend Developer</h2>
+      <div className="bio">
+        <p>When I'm not designing and developing websites, I like to write stories and blog posts, sketch from life and my imagination, and occasionally create music. My main passion is learning, because it allows me to execute bigger, better, more ambitious creations.</p>
+
+        <p>Some of my most notable accompliishments include working on a Best in Show award-winning website, writing two novels (unpublished), interviewing a chart-topping musician, and writing hundreds of blog posts over the past six years.</p>
+
+        <p>Mostly I just play with JavaScript, though.</p>
+      </div>
+      <SocialIcons />
+    </div>
+    <div className="transition-shape">
+      <div className="trapezoid"></div>
+      <div className="trapezoid"></div>
+      <div className="trapezoid"></div>
+    </div>
+  </section>
 </template>
 
 <script>
-export default {
-  name: 'about'
-}
+  import SocialIcons from './SocialIcons'
+  export default {
+    name: 'about',
+    components: {
+      SocialIcons
+    }
+  }
 </script>
 
-<style>
+<style scoped lang="sass">
+  @import '../styles/variables'
+
+  $shape-height: 4em
+  $shape-width: 5em
+
+  .about
+    position: relative
+    color: $color-text-dark
+    background-color: $color-text-light
+    padding-bottom: 0
+
+  .about + section
+    margin-top: 0
+
+  .about .social-icons
+    position: absolute
+    left: 50%
+    bottom: 0
+    transform: translateX(-50%) translateY(70%)
+    z-index: 5
+
+  .h-card
+    position: relative
+    opacity: .85
+    padding-bottom: 2em
+    z-index: 3
+
+  .transition-shape
+    display: flex
+    position: absolute
+    width: 100%
+    bottom: 0
+    left: 0
+    transform: translateY(100%)
+
+  .trapezoid
+    border-top: $shape-height solid $color-text-light
+    border-left: $shape-width solid transparent
+    border-right: $shape-width solid transparent
+    height: 0
+    width: 60%
+
+  .trapezoid:first-child,
+  .trapezoid:last-child
+    width: calc(40% / 2)
+
+  .trapezoid:first-child:before
+    content: ''
+    display: block
+    position: absolute
+    left: 0
+    top: 0
+    width: 0
+    height: 0
+    border-bottom: $shape-height solid $color-text-light
+    border-right: $shape-width solid transparent
+
+  .trapezoid:last-child:after
+    content: ''
+    display: block
+    position: absolute
+    right: 0
+    top: 0
+    width: 0
+    height: 0
+    border-bottom: $shape-height solid $color-text-light
+    border-left: $shape-width solid transparent
 </style>
