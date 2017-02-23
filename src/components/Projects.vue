ffb105f12d35e137e3ab29934ec9916af181b47f
Mike Cornish <cornishmw@gmail.com>
2017-02-22 19:17:42 -0500

    Bring Projects component to working state

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -19,15 +19,14 @@
         <p>{{project.description}}</p>
       </div>
     </div>
-    <button @click="showGrid">More Projects</button>
-    <div role="grid">
+    <button @click="toggleGrid">More Projects</button>
+    <div v-show="hasGrid" role="grid">
       <a
         v-for="project in projects"
         role="gridcell"
         :href="project.link"
         target="_blank"
         class="project"
-        :class="{'is-active': isActive(project)}"
         @click="trackClick(project.name)"
       >
         <img 
@@ -45,26 +44,107 @@
 </div>
 </template>
 
+
 <script>
-// import _ from 'lodash'
+import _ from 'lodash'
 import projects from '../project-data'
 // import { mixpanel } from '../main'
 
 export default {
-  name: '',
+  name: 'projects',
   components: {
   },
 
   data () {
     return {
-      projects
+      projects,
+      hasGrid: false
+    }
+  },
+
+  computed: {
+    featured () {
+      const isFeatured = _.partial(_.has, _, 'featured')
+      return this.projects.filter(isFeatured)
     }
   },
 
   methods: {
+    toggleGrid () {
+      this.hasGrid = !this.hasGrid
+    }
   }
 }
 </script>
 
+
 <style scoped lang="sass">
+@import '../styles/variables'
+
+.projects
+  width: 90%
+
+.project
+  position: relative
+  will-change: transform
+  transform: none
+  transition: transform .2s, margin .2s
+  @media screen and (min-width: 1px)
+    width: calc(100% - 2em)
+  @media screen and (min-width: $break-tablet)
+    width: calc(50% - 2em)
+  @media screen and (min-width: $break-desktop)
+    width: calc(25% - 2em)
+
+.project.is-active,
+.project:hover
+  transform: scale(1.1)
+  z-index: 1
+
+.project__thumbnail
+  width: 100%
+  height: 100%
+  border: 1px solid rgba(0,0,0,.2)
+
+.project__placeholder
+  display: flex
+  justify-content: center
+  align-items: center
+  width: 100%
+  height: 12em
+  background-color: rgb(230,230,230)
+
+.project__placeholder span
+  color: rgb(150,150,150)
+
+.project__name
+  position: absolute
+  left: 50%
+  top: 50%
+  color: rgb(250,250,250)
+  transform: translate(-50%, -50%)
+  opacity: 0
+  transition: opacity .2s
+  z-index: 1
+
+.project:hover .project__name,
+.is-active .project__name
+  opacity: 1
+
+.project__shade
+  background-color: mix($color-brand-1, black, 30);
+  opacity: 0
+  position: absolute
+  top: 0
+  left: 0
+  width: 100%
+  height: 100%
+  transition: opacity .2s
+
+.project:nth-child(2n) .project__shade
+  background-color: mix($color-brand-2, black, 30);
+
+.project:hover .project__shade,
+.is-active .project__shade
+  opacity: .7
 </style>
---
+++ src/project-data.js
@@ -0,0 +1,138 @@
+import _ from 'lodash'
+import shortid from 'shortid'
+import blogThumb from './assets/blog-thumb.jpg'
+import novelThumb from './assets/novel-thumb.jpg'
+import timestampThumb from './assets/timestamp-thumb.jpg'
+import headerThumb from './assets/header-thumb.jpg'
+import shortenerThumb from './assets/shortener-thumb.jpg'
+import searchThumb from './assets/search-thumb.jpg'
+
+const projects = [
+  {
+    'name': 'devStory Blog',
+    'thumbnail': blogThumb,
+    'link': 'http://devstory.mikecornish.net',
+    'tools': ['HTML5', 'CSS3'],
+    'created_on': new Date('6/16/15'),
+    'featured': true
+  },
+  {
+    'name': 'Novel Website',
+    'thumbnail': novelThumb,
+    'link': 'http://novel.mikecornish.net',
+    'tools': ['Angular', 'HTML5', 'CSS3'],
+    'created_on': new Date('9/1/16')
+  },
+  {
+    'name': 'Weather App',
+    'thumbnail': null,
+    'codepenId': 'pgmZXo',
+    'tools': ['HTML5', 'CSS3', 'Sass'],
+    'created_on': new Date('2/19/16')
+  },
+  {
+    'name': 'Wikipedia Viewer',
+    'thumbnail': null,
+    'codepenId': 'WrqBPE',
+    'tools': ['HTML5', 'CSS3', 'Sass'],
+    'created_on': new Date('2/22/16')
+  },
+  {
+    'name': 'Javascript Calculator',
+    'thumbnail': null,
+    'codepenId': 'jqNwvx',
+    'tools': ['HTML5', 'CSS3', 'Sass'],
+    'created_on': new Date('2/25/16')
+  },
+  {
+    'name': 'Simon Game',
+    'thumnail': null,
+    'codepenId': 'xOZjXA',
+    'tools': ['HTML5', 'CSS3', 'Sass'],
+    'created_on': new Date('8/19/16'),
+    'featured': true
+  },
+  {
+    'name': 'Markdown Previewer',
+    'thumbnail': null,
+    'codepenId': 'zKObRp',
+    'tools': ['HTML5', 'CSS3', 'Sass', 'React'],
+    'created_on': new Date('8/31/16')
+  },
+  {
+    'name': 'Free Code Camp Leaderboard',
+    'thumbnail': null,
+    'codepenId': 'pEJPbL',
+    'tools': ['HTML5', 'CSS3', 'Sass', 'React'],
+    'created_on': new Date('9/7/16')
+  },
+  {
+    'name': 'Recipe List with Local Storage',
+    'thumbnail': null,
+    'codepenId': 'bwVxLG',
+    'tools': ['HTML5', 'CSS3', 'Sass', 'React'],
+    'created_on': new Date('9/11/16')
+  },
+  {
+    'name': 'Bar Chart with D3.js',
+    'thumbnail': null,
+    'codepenId': 'WGGZaQ',
+    'tools': ['HTML5', 'CSS3', 'Sass', 'D3'],
+    'created_on': new Date('9/18/16')
+  },
+  {
+    'name': 'Scatterplot with D3.js',
+    'thumbnail': null,
+    'codepenId': 'vXgNAr',
+    'tools': ['HTML5', 'CSS3', 'Sass', 'D3'],
+    'created_on': new Date('9/22/16')
+  },
+  {
+    'name': 'Timestamp Microservice',
+    'thumbnail': timestampThumb,
+    'link': 'http://mctimestamp.herokuapp.com',
+    'tools': ['HTML5', 'CSS3', 'Express', 'Node'],
+    'created_on': new Date('9/24/16')
+  },
+  {
+    'name': 'Header Parser Microservice',
+    'thumbnail': headerThumb,
+    'link': 'http://mc-header-parser.herokuapp.com/',
+    'tools': ['HTML5', 'CSS3', 'Express', 'Node'],
+    'created_on': new Date('10/3/16')
+  },
+  {
+    'name': 'URL Shortener Microservice',
+    'thumbnail': shortenerThumb,
+    'link': 'http://mcshort.herokuapp.com/',
+    'tools': ['HTML5', 'CSS3', 'Express', 'Node'],
+    'created_on': new Date('10/4/16'),
+    'featured': true
+  },
+  {
+    'name': 'Image Search Microservice',
+    'thumbnail': searchThumb,
+    'link': 'http://mcsearch.herokuapp.com/',
+    'tools': ['HTML5', 'CSS3', 'Express', 'Node'],
+    'created_on': new Date('10/6/16')
+  }
+]
+
+// Most recent date gets rendered first
+const dateSort = (a, b) => {
+  if (a.created_on < b.created_on) {
+    return 1
+  } else if (b.created_on < a.created_on) {
+    return -1
+  } else {
+    return 0
+  }
+}
+
+const addId = project => {
+  const id = shortid.generate()
+  const newProj = _.assign(project, {id})
+  return newProj
+}
+
+export default projects.sort(dateSort).map(addId)
